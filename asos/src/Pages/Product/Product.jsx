7a3fe77724d4cbe89eb8d5aa1ef8048b3b1539d3
Kundan Kumar <kd181197@gmail.com>
2022-06-15 15:02:11 +0530

    product footer dome

--- asos/src/Pages/Product/Product.jsx
+++ asos/src/Pages/Product/Product.jsx
@@ -47,63 +47,94 @@ const Product = () => {
   }
 
   return (
-    <div className={styles.pBody}>
-      <div className={styles.sideImage}>
-        {SliderData.map((el) => {
-          return <img src={el.image} alt="" />;
-        })}
+    <div>
+      <div className={styles.pBody}>
+        <div className={styles.sideImage}>
+          {SliderData.map((el) => {
+            return <img src={el.image} alt="" />;
+          })}
+        </div>
+        <section className={styles.slider}>
+          <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+          {SliderData.map((slide, index) => {
+            return (
+              <div
+                className={
+                  index === current ? styles.slide.active : styles.slide
+                }
+                key={index}
+              >
+                {index === current && (
+                  <img
+                    src={slide.image}
+                    alt="travel image"
+                    className={styles.image}
+                  />
+                )}
+              </div>
+            );
+          })}
+          <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+        </section>
+        <div className={styles.pDetails}>
+          <div>
+            <p>{name}</p>
+            <p>
+              RRP{oldPrice} <b style={{ color: "red" }}>Now{newPrice}</b>(-40%)
+            </p>
+          </div>
+          <p>COLOR:{color}</p>
+          <label htmlFor="">SIZE</label>
+          <select name="" id="">
+            <option value="..">Please select</option>
+            <option value="">S</option>
+            <option value="">M</option>
+            <option value="">L</option>
+            <option value="">XL</option>
+          </select>
+          <div>
+            <button>ADD TO BAG </button>
+            <FaRegHeart />
+          </div>
+          <div>
+            <FiTruck /> <span>Free Delevery.</span>
+            <p>
+              Ts&Cs apply. <u>Learn more</u>
+            </p>
+          </div>
+        </div>
       </div>
-
-      <section className={styles.slider}>
-        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
-        {SliderData.map((slide, index) => {
-          return (
-            <div
-              className={index === current ? styles.slide.active : styles.slide}
-              key={index}
-            >
-              {index === current && (
-                <img
-                  src={slide.image}
-                  alt="travel image"
-                  className={styles.image}
-                />
-              )}
+      <div className={styles.extraD}>
+            <div>
+                <h3>PRODUCT DETAILS</h3>
+                <p><u>Blouse by JDY</u></p>
+                <ul>
+                    <li>The scroll is over</li>
+                    <li> V-neck</li>
+                    <li>Button plackt</li>
+                    <li>Long sleaves</li>
+                </ul>
             </div>
-          );
-        })}
-        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
-      </section>
-
-      <div className={styles.pDetails}>
-        <div>
-          <p>{name}</p>
-          <p>
-            RRP{oldPrice}{" "}
-           
-              <b style={{color:"red"}}>Now{newPrice}</b>(-40%)
-            
-          </p>
-        </div>
-        <p>COLOR:{color}</p>
-        <label htmlFor="">SIZE</label>
-        <select name="" id="">
-          <option value="..">Please select</option>
-          <option value="">S</option>
-          <option value="">M</option>
-          <option value="">L</option>
-          <option value="">XL</option>
-        </select>
-        <div>
-        <button>ADD TO BAG </button>
-        <FaRegHeart />
-        </div>
-        
-        <div >
-         <FiTruck/> <span>Free Delevery.</span>
-         <p>Ts&Cs apply. <u>Learn more</u></p>
-        </div>
+            <div>
+                <h3>PRODUCT CODE</h3>
+                <p>120368583</p>
+                <h3>BRAND</h3>
+                <p>{name}</p>
+            </div>
+            <div>
+                <h3>SIZE & FIT</h3>
+                <p>Model wears: UK 8/ EU 36/ US 4</p>
+                <p>Model's height: 170cm/5'7"</p>
+            </div>
       </div>
+     <div className={styles.footCarousal}>
+          <h3>Recently viewed</h3>
+          <div>
+            {SliderData.map((el)=>{
+             return   <img src={el.image} alt="" />
+            })}
+          </div>
+     </div>
     </div>
   );
 };
