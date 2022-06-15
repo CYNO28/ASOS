363431a4485158c650796432a3d040bd92fd1fcf
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-06-15 20:27:47 +0530

    Merge branch 'main' of https://github.com/CYNO28/ASOS

--- asos/src/Pages/Mens/Men.jsx
+++ asos/src/Pages/Mens/Men.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import style from '../../Styles/Pages/Men.module.css'
+import style from './Men.module.css'
 const Men = () => {
-  const background =
-    "https://content.asos-media.com/-/media/homepages/unisex/homepages/2021/12_dec/sale/xmas-sale-2021/single-gradient/1/desktop.jpg";
+  
 const saleimg="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/sale-lockups/sale/dt-white/215_40_dt_sale_white_uk_rosv2.png"
 const secbackground="https://content.asos-media.com/-/media/homepages/mw/2022/june/06/hero/mw1_dt_hero_1258x600.jpg"
 const secbackgroundsmall='https://content.asos-media.com/-/media/homepages/mw/2022/june/06/hero/mw_1mob_hero_640x692.jpg'
@@ -10,20 +9,21 @@ const bannerimg="https://content.asos-media.com/-/media/homepages/unisex/homepag
   return (
     <div>
       Men
-      <div className="Top-section">
-        <div className="sale-img"></div>
-        <div className="sale-span">
+      <div className={style.top_section}>
+        <div className={style.sale_img}> <img src={saleimg} alt="" /></div>
+        <div >
             <span>
-                UP TO 70% OFF!
-                <br />
+               <h2 className={style.salespan}>UP TO 70% OFF!
+               <br />
                 IT'S CALLED BIG DISCOUNT ENERGY
-            </span>
+            </h2> 
+                </span>
         </div>
         <div className="crousel"></div>
         <div className="view-all-btn">
-            <button>VIEW ALL</button>
+            <button className={style.middlebtn}>VIEW ALL</button>
         </div>
-        <p> Limited time only. Selected styles marked down as shown.</p>
+        <p className={style.topdis}> Limited time only. Selected styles marked down as shown.</p>
       </div>
     </div>
   );
