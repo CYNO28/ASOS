f4a8f26f9044716db8ad90e859ab3fb68ab47bde
gauravKverma <gauravverma004@gmail.com>
2022-06-16 13:25:26 +0530

    JoinPage

---
+++ asos/src/Pages/Join Page/JoinPage.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import Join from '../Join/Join'
+import styles from "./joinpage.module.css"
+import SingIn from '../SignIn/SingIn'
+
+const JoinPage = () => {
+    const [show, setShow] = useState(true)
+  return (
+    <div>
+        <div className={styles.pageDiv}>
+            <h1 className={styles.title}>asos</h1>
+            <div className={styles.joinSection}>
+                <div className={styles.topButtonsDiv}>
+                    <button onClick={() => setShow(true)} className={styles.topButton}>
+                        JOIN
+                    </button>
+                    <div className={styles.vl}></div>
+                    <button onClick={() => setShow(false)} className={styles.topButton}>
+                        SIGN IN
+                    </button>
+                </div>
+            </div>
+            <div>
+                {show ? <Join/> : <SingIn/>}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default JoinPage
