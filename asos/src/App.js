c517d0ac977ef87d6319fe999b744c46c48f6205
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-06-17 20:08:30 +0530

    1

--- asos/src/App.js
+++ asos/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import SubNav from './component/subNavbar/SubNav';
 import Men from './Pages/Mens/Men';
-import Product from './Pages/Product/Product';
+import Product from './Pages/ProductDetail/Product';
 import Footer from './component/footer/Footer'
 import Women from './Pages/Women/Women';
 
 function App() {
   return (
     <div className="">
+<SubNav/>
 
-
-<Footer/>
+{/* <Footer/> */}
     </div>
   );
 }
