5905a98b8aa85efba7dcc7458e04fc24fc4c4fcd
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-06-17 20:18:35 +0530

    q11

---
+++ asos/src/Pages/Products/ProductsM.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProductsM = () => {
+  return (
+    <div>
+        <BreadCrumb></BreadCrumb>
+      <div className={style.gridbox}>
+        <div className={style.imgbox2}></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsM;
