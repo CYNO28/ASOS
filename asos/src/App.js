7e3d73ee942a463865cc632ebbae68c930159ab0
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-06-18 10:49:55 +0530

    1

--- asos/src/App.js
+++ asos/src/App.js
@@ -4,13 +4,25 @@ import Men from './Pages/Mens/Men';
 import Product from './Pages/ProductDetail/Product';
 import Footer from './component/footer/Footer'
 import Women from './Pages/Women/Women';
+import { Route, Routes } from 'react-router-dom';
+import Join from './Pages/Join/Join';
+import JoinPage from './Pages/Join Page/JoinPage';
+import SingIn from './Pages/SignIn/SingIn';
 
 
 function App() {
   return (
-    <div className="">
+      <div className="">
+        <Routes>
+          <Route path={'/'} element={<></>}></Route>
+          <Route path={'/men'} element={<Men></Men>}></Route>
+          <Route path={'/wome'} element={<Women></Women>}></Route>
+          <Route path={'/join'} element={<Join></Join>}></Route>
+          {/* <Route path={'/'} element={<></>}></Route> */}
 
-    </div>
+        </Routes>
+
+      </div>
   );
 }
 
