11dc84a484e0a4e38dae511a76735c9941d0e281
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-06-18 20:03:25 +0530

    1

--- asos/src/App.js
+++ asos/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import SubNav from './component/subNavbar/SubNav';
+import Navbar from './component/Navbar/Navbar';
 import Men from './Pages/Mens/Men';
 import Product from './Pages/ProductDetail/Product';
 import Footer from './component/footer/Footer'
 import Women from './Pages/Women/Women';
 import { Route, Routes } from 'react-router-dom';
 import Join from './Pages/Join/Join';
-import JoinPage from './Pages/Join Page/JoinPage';
+// import JoinPage from './Pages/J/oin Page/JoinPage';
 import SingIn from './Pages/SignIn/SingIn';
-import Navbar from './component/Navbar/Navbar';
 import Home from './Pages/home/Home';
-import Cart from './Pages/Cart/Cart';
+// import Cart from './Pages/Cart/Cart';
 
 
 function App() {
@@ -20,10 +20,10 @@ function App() {
   
         <Routes>
           <Route path={'/'} element={<Home></Home>}></Route>
-          <Route path={'/men'} element={<Men></Men>}></Route>
-          <Route path={'/women'} element={<Women></Women>}></Route>
+          <Route path={'/men'} element={<Men></Men>}></Route>/
+          <Route path={'/women'} element={<Women></Women>}></Route> 
           <Route path={'/join'} element={<Join></Join>}></Route>
-          <Route path={'/cart'} element={<Cart></Cart>}></Route>
+           {/* <Route path={'/cart'} element={<Cart></Cart>}></Route> */}
           {/* <Route path={'/'} element={<></>}></Route> */}
 
         </Routes>
